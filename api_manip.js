81ababf7741d282f3a9c2cd5d27c140913781071
rvferreira <raphael.rvf@gmail.com>
2015-09-05 23:11:13 -0300

    window update verification mode

--- api_manip.js
+++ api_manip.js
@@ -1,50 +1,41 @@
+function parseWebPage(url, callback) {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange=function()
+	{
+		if (xmlHttp.readyState == 4)
+		{
+			callback(xmlHttp.responseText);
+		}
+	}
+
+	xmlHttp.open("GET", url, true); 
+	xmlHttp.send();
+}
+
 function requestToken(callback){
 	chrome.windows.create(
 
-		{'url' : 'https://www.facebook.com/dialog/oauth?'
-			+ "display=popup&"
-			+ "client_id=1007723535938990&"
-			+ "redirect_uri=https://www.google.com&"
-			+ "scope=publish_actions&response_type=token",
+		{'url' : 'https://sso.godaddy.com/login?realm=idp&app=shortener&path=/',
 			'width' : 580,
 			'height' : 400
 		},
 
 		function(popupWindow){
+
 			chrome.tabs.query(
 				{active : true},
 				function(tabs){
-					var tab_id = tabs[0].id;
-					chrome.tabs.onUpdated.addListener(
-						function(tab_id, tab) {
-
-							var tabUrl = tab.url;
-							var accessTokenMatcher = null;
-							var expiresInMatcher = null;
-							
-							if (tabUrl != null) {
-								accessTokenMatcher = tabUrl.match(/[\\?&#]access_token=([^&#])*/i);
-								expiresInMatcher = tabUrl.match(/expires_in=.*/);
-							}
-							
-							if (accessTokenMatcher != null) {
-								token = accessTokenMatcher[0];
-								token = token.substring(14);
-								expires_in = expiresInMatcher[0];
-								expires_in = expires_in.substring(11);
-
-								localStorage.accessToken = token;
-								
-								var currentDate = new Date();
-								var expiryTime = currentDate.getTime() + 1000 * (expires_in - 300);
-
-								localStorage.expiryTime = expiryTime;
-								chrome.windows.remove(popupWindow.id);
-								callback();
-						}
+					chrome.tabs.onUpdated.addListener(function fetchAPIKey(){
+						chrome.tabs.onUpdated.removeListener(fetchAPIKey);
+						parseWebPage("https://shortener.godaddy.com/v1/apikey", function(res){
+							localStorage.APIKey = res;
+						});
+						chrome.windows.remove(popupWindow.id);
+						callback();
 					});
 				}
 			);
+			
 		}
 	);
 }
--- popup.js
+++ popup.js
@@ -1,13 +1,12 @@
 function set_iframe_content(){ //TODO remove this function
-	console.log((localStorage.accessToken));
+	console.log((localStorage.APIKey));
 	$('#ext-main').show();
-	$('#ext-main').attr('src', 'https://shortener.godaddy.com');	
+	// $('#ext-main').attr('src', 'http://anycom.herokuapp.com?url=&#8221');	
 }
 
 function startApp(){
 
 	chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
-		//if (localStorage.accessToken){} //TODO
 		$('#ext-main').hide();
 		requestToken(set_iframe_content);
 	});
@@ -16,3 +15,4 @@ function startApp(){
 document.addEventListener('DOMContentLoaded', function(){ 
 	startApp();
 });
+
